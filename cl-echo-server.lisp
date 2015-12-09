71ea46878039f1968324e1c8e6798c145555d055
Smith Dhumbumroong <zodmaner@gmail.com>
2015-12-09 23:57:32 +0700

    Minor refactors and adds/extends docstrings

--- cl-echo-server.lisp
+++ cl-echo-server.lisp
@@ -3,30 +3,31 @@
 (in-package #:cl-echo-client-server)
 
 (defun echo-server (port &key (hostname "127.0.0.1"))
-  "A simple echo server."
+  "A simple multi-threaded echo server."
   (let* ((passive-socket (socket-listen hostname port :reuse-address t))
          (server-thread (make-thread
                          #'(lambda ()
                              (loop
                                 (accept-and-handle passive-socket)))
-                         :name "server-thread")))
+                         :name "echo-server-thread")))
     #'(lambda ()
         (socket-close passive-socket)
         (destroy-thread server-thread))))
 
 (defun accept-and-handle (passive-socket)
-  "Accepts and handles a connection."
-  (let* ((active-socket (socket-accept passive-socket))
-         (stream (socket-stream active-socket)))
+  "Accepts and creates a new thread to handle a connection."
+  (let ((active-socket (socket-accept passive-socket)))
     (make-thread
      #'(lambda ()
-         (handle stream)
-         (close stream)
-         (socket-close active-socket)))))
+         (with-open-stream (stream (socket-stream active-socket))
+           (handle stream))
+         (socket-close active-socket))
+     :name "handler-thread")))
 
 (defun handle (stream)
-  "Reads, processes, and writes data back to the client."
+  "Reads, processes, and writes data back to the client.
+This function will run until a client disconnects."
   (do ((echo-text (read-line stream nil) (read-line stream nil)))
-      ((null echo-text))
+      ((null echo-text)) ; loops until the client disconnects
     (format stream "echo: ~A~%" echo-text)
     (force-output stream)))
