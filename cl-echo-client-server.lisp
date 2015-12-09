b95d5c9ee5e1189a0a5d1e0056aae7e19d92964e
Smith Dhumbumroong <zodmaner@gmail.com>
2015-12-10 01:27:54 +0700

    Rewrites the example

--- cl-echo-client-server.lisp
+++ cl-echo-client-server.lisp
@@ -4,9 +4,10 @@
 
 ;;; "cl-echo-client-server" goes here. Hacks and glory await!
 
+;; usage example
+
 ;; starts the echo server
 (defparameter *stop-echo-server* (echo-server 8080)
   "A lexical closure/function that can be used (via funcall) to cleanly shutdown the echo server.")
 
-;; starts the echo client
-(echo-client 8080)
+;; the echo client can be started by invoke the echo-client function
